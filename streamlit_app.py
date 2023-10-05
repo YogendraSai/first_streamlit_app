8d97600e5b7dc754f680c5455f69e2b5f161deed
Yogendra Sai Mummareddy <42644251+YogendraSai@users.noreply.github.com>
2023-10-05 14:34:35 -0700

    Update streamlit_app.py
    Added pickup input and displayed the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas as pd
 
-streamlit.title(' My Parents New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 
@@ -11,5 +11,14 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#Adding Csv file to the streamlit app
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
